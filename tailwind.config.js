1591adf10fcbb3cb872bf74dcc12b71c5af97759
Thomas De Grava <thomas_alexandre96@hotmail.com>
2023-09-13 21:11:08 -0300

    setup configuration

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    colors: {
+      'black': '#000',
+      'white': '#FFF',
+      'opacity-black': '#00000066',
+      'light-black': '#2E2E2E',
+      'almost-black': '#18191F',
+      'light-gray': '#808080',
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Manrope', 'Arial', 'sans-serif'],
+      },
+    }
+  }
+}
